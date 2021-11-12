fc54f62f5c6a8472c09456ea3a2ce8191df26bec
dashabobrova <work.dbobrova.dasha@gmail.com>
2021-11-12 18:13:13 +0300

    default popup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { useState } from "react";
-import "./App.css";
+import "./App.scss";
 import { NewPopup } from "./components/NewPopup";
 import { DialogContent, DialogContentText } from "@material-ui/core";
 
 function App() {
   const [open, setOpen] = useState(false);
-
+  const togglerClickHandler = () => {
+    setOpen(!open);
+  };
   return (
     <div className="App">
+      <button onClick={togglerClickHandler}>Open</button>
       <NewPopup
         open={open}
+        togglerClickHandler={togglerClickHandler}
         title="Titile-Popup1"
         setOpen={setOpen}
         content={
--- src/components/NewPopup.jsx
+++ src/components/NewPopup.jsx
@@ -1,67 +1,35 @@
 import * as React from "react";
-import {
-  Dialog,
-  Button,
-  DialogTitle,
-  DialogContent,
-  makeStyles
-} from "@material-ui/core";
-import IconButton from "@material-ui/core/IconButton";
-import SvgIcon from "@material-ui/core/SvgIcon";
+import { Dialog, makeStyles } from "@material-ui/core";
+import CrossSvg from "../img/Cross.svg"; // CUicon-interface-Cross
 
 const useStyles = makeStyles({
-    dialog: {
-      position: 'absolute',
-      width: 400,
-      minHeight: 210,
-    }
-  });
-  
+  position: "absolute",
+  minWidth: 400,
+  minHeight: 210,
+  borderRadius: 16,
+});
 
-export const NewPopup = ({ title, setOpen, open, content }) => {
-  const togglerGlickHandler = () => {
-    setOpen(!open);
-  };
-
+export const NewPopup = ({ title, open, content, togglerClickHandler }) => {
   const classes = useStyles();
 
   return (
-    <div>
-      <Button variant="outlined" onClick={togglerGlickHandler}>
-        Open
-      </Button>
-      <Dialog
-      classes={{
-        paper: classes.dialog
-      }}
-
-        open={open}
-        onClose={togglerGlickHandler}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
-          <IconButton aria-label="Delete" onClick={togglerGlickHandler}>
-            <SvgIcon>
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M15.5899 0.410068C16.1367 0.956825 16.1367 1.84329 15.5899 2.39005L2.39005 15.5899C1.84329 16.1367 0.956825 16.1367 0.410068 15.5899C-0.136689 15.0432 -0.136689 14.1567 0.410068 13.61L13.61 0.410068C14.1567 -0.136689 15.0432 -0.136689 15.5899 0.410068Z"
-                fill="#0E46C0"
-              />
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M15.5899 15.5899C15.0432 16.1367 14.1567 16.1367 13.6099 15.5899L0.410067 2.39005C-0.136689 1.84329 -0.136689 0.956824 0.410068 0.410067C0.956825 -0.13669 1.84329 -0.136689 2.39005 0.410068L15.5899 13.61C16.1367 14.1567 16.1367 15.0432 15.5899 15.5899Z"
-                fill="#0E46C0"
-              />
-            </SvgIcon>
-          </IconButton>
-        <DialogContent>
-         {content}
-        </DialogContent>
-        
-      </Dialog>
-    </div>
+    <Dialog
+      classes={{ paper: classes }}
+      open={open}
+      onClose={togglerClickHandler}
+    >
+      <div className="popup__wrapper">
+        <div className="popup__header">
+          <h1 className="popup__header-title">{title}</h1>
+          <img
+            className="popup__header-cross"
+            src={CrossSvg}
+            alt="x"
+            onClick={togglerClickHandler}
+          />
+        </div>
+        <div className="popup__content">{content}</div>
+      </div>
+    </Dialog>
   );
 };
