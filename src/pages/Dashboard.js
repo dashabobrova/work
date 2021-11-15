1cbbaba44f8c89fb56b89429e232900dfefed13d
dashabobrova <work.dbobrova.dasha@gmail.com>
2021-11-15 17:53:30 +0300

    position dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -9,17 +9,22 @@ export const Dashboard = () => {
       <div className="blueBlock">
         <div className="blueBlock__content">
           <div className="blueBlock__main">
-              <div className="blueBlock__main-wrapper">
-                  <div>
-                  </div>
-                  <div></div>
+            <div className="blueBlock__main-blocks">
+              <div className="blueBlock__main-left">
+              <div className="left__header">
+                  <p className="CUicon-solid_general-Wallet left__header-wallet">Кошелек</p>
+                  <button className="left__header-btn ">Перейти в раздел<span className="CUicon-solid_interface-Caret-right"></span></button>
+                </div>
               </div>
+              <div className="blueBlock__main-right"></div>
+            </div>
+            <div className="blueBlock__main-btns"></div>
           </div>
+
           <div className="blueBlock__small">
-          <div className="blueBlock__small-main">sec</div>
-          <div className="blueBlock__small-battery"></div>
+            <div className="blueBlock__small-main">sec</div>
+            <div className="blueBlock__small-battery"></div>
           </div>
-          
         </div>
       </div>
     </div>
